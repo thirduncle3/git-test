12988a85e9656bc675ca2b11992bd9867c478e81
三叔 <100853441+thirduncle3@users.noreply.github.com>
2022-11-04 17:42:14 +0800

    Update GitTest.java

--- src/main/java/com/thirduncle/GitTest.java
+++ src/main/java/com/thirduncle/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello hot-fix git3!");
         System.out.println("hello github git3!");
         System.out.println("hello push git3!");
+        System.out.println("hello push git3! 更新了一下");
     }
 }
