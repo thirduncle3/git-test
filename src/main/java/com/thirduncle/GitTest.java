df2fc30668076533a7b8bf4b5fdcaade668608aa
thirduncle <2284027823@qq.com>
2022-11-04 14:49:01 +0800

    second commit

--- src/main/java/com/thirduncle/GitTest.java
+++ src/main/java/com/thirduncle/GitTest.java
@@ -7,5 +7,6 @@ package com.thirduncle;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
