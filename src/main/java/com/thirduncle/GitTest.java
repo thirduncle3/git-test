50a854ecfa94f127c8f8a19b1813102ce98b37d7
thirduncle <2284027823@qq.com>
2022-11-04 14:47:31 +0800

    first commit

---
+++ src/main/java/com/thirduncle/GitTest.java
@@ -0,0 +1,11 @@
+package com.thirduncle;
+
+/**
+ * @author Third uncle
+ * @date 2022/11/4 - 14:45
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
