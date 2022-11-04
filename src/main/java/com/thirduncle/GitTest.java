1163a98b09334b2077a62d4a622411da59cf7003
三叔 <100853441+thirduncle3@users.noreply.github.com>
2022-11-04 16:21:46 +0800

    github commit

--- src/main/java/com/thirduncle/GitTest.java
+++ src/main/java/com/thirduncle/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("hello master git4!");
         System.out.println("hello hot-fix git3!");
+        System.out.println("hello github git3!");
     }
 }
